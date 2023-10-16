3927986032ed497c40c5b732b6898c0d187d5e05
lionkingchuja <94731498+lionkingchuja@users.noreply.github.com>
2023-10-16 18:17:01 +0530

    Time: 95 ms (100%), Space: 80 MB (70%) - LeetHub

--- 3170-find-indices-with-index-and-value-difference-ii/3170-find-indices-with-index-and-value-difference-ii.cpp
+++ 3170-find-indices-with-index-and-value-difference-ii/3170-find-indices-with-index-and-value-difference-ii.cpp
@@ -2,26 +2,15 @@ class Solution {
 public:
     vector<int> findIndices(vector<int>& nums, int id, int vd) {
         int n=nums.size();
-        vector<pair<int,int>>vp(n);
-        vp[n-1].first=nums[n-1];
-        vp[n-1].second=nums[n-1];
-        for(int i=n-2;i>=0;i--){
-            vp[i].first=max(nums[i],vp[i+1].first);
-            vp[i].second=min(nums[i],vp[i+1].second);
+        int mini=0;
+        for(int i=0;i<n-id;i++){
+            if(nums[i] <nums[mini]) mini=i;
+            if(abs(nums[mini]-nums[i+id])>=vd) return{mini,i+id};
         }
-        vector<int>ans(2,-1);
-        for(int i=0;i<n;i++){
-            int j=i+id;
-            if(j<n and (abs(nums[i]-vp[j].first) >=vd or abs(nums[i]-vp[j].second) >=vd)){
-                ans[0]=i;
-                for(int k=j;k<n;k++){
-                    if(abs(nums[i]-nums[k])>=vd){
-                        ans[1]=k;
-                        return ans;
-                    }
-                }
-                //break;
-            }
+        int maxi=0;
+        for(int i=0;i<n-id;i++){
+            if(nums[i] >nums[maxi]) maxi=i;
+            if(abs(nums[maxi]-nums[i+id])>=vd) return{maxi,i+id};
         }
         return {-1,-1};
     }
