9158814cad4b1040033b10513c1192ebd990f841
Vivek Kumar <94731498+lionkingchuja@users.noreply.github.com>
2024-06-16 17:54:36 +0530

    Time: 11 ms (21.31%), Space: 13.7 MB (45.41%) - LeetHub

---
+++ 0330-patching-array/0330-patching-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minPatches(vector<int>& nums, int n) {
+        int i=0;
+        long long sum=1;
+        int ans=0;
+        while(sum <=n){
+            if(i<nums.size() and nums[i] <= sum) {
+                sum +=nums[i++];
+                cout<<sum<<endl;
+            }
+            else sum +=sum;
+            ans++;
+        }
+        cout<<ans<<" "<<i;
+        return ans-i;
+    }
+};
