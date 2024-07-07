7822df447bb26d4dc6ec28ff84174d6e21c4f878
Vivek Kumar <94731498+lionkingchuja@users.noreply.github.com>
2024-07-07 18:15:08 +0530

    Time: 942 ms (55.7%), Space: 0B (100%) - LeetHub

--- 1541-top-travellers/1541-top-travellers.sql
+++ 1541-top-travellers/1541-top-travellers.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-select t1.name,IFNULL(sum(t2.distance),0) as travelled_distance
+select t1.name,IF(sum(t2.distance)is NULL,0,sum(t2.distance)) as travelled_distance
 from Rides as t2 right join Users as t1
 on t1.id=t2.user_id
 group by t2.user_id
