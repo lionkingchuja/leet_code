2469d28c4240af7ad7bbbca1a8656ac245a73feb
Vivek Kumar <94731498+lionkingchuja@users.noreply.github.com>
2024-07-11 19:36:10 +0530

    Time: 1023 ms (94.06%), Space: 0B (100%) - LeetHub

---
+++ 1268-market-analysis-i/1268-market-analysis-i.sql
@@ -0,0 +1,12 @@
+SELECT 
+    t1.user_id as buyer_id,
+    t1.join_date,
+    COUNT(CASE WHEN YEAR(t2.order_date) = 2019 THEN 1 ELSE NULL END) AS orders_in_2019
+FROM 
+    Users AS t1
+LEFT JOIN 
+    Orders AS t2 
+ON 
+    t1.user_id = t2.buyer_id
+GROUP BY 
+    t1.user_id, t1.join_date;
