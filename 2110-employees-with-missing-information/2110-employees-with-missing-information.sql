14fb8f79eee528b716ba05d24a744559bda5a6d4
Vivek Kumar <94731498+lionkingchuja@users.noreply.github.com>
2024-07-12 23:40:49 +0530

    Time: 560 ms (56.52%), Space: 0B (100%) - LeetHub

---
+++ 2110-employees-with-missing-information/2110-employees-with-missing-information.sql
@@ -0,0 +1,17 @@
+# Write your MySQL query statement below
+with new as
+(
+    select t1.employee_id
+    from Employees as t1 left join Salaries as t2 
+    on t1.employee_id=t2.employee_id
+    where t1.name is null or t2.salary is null
+    union 
+    select t2.employee_id
+    from Employees as t1 right join Salaries as t2
+    on t1.employee_id=t2.employee_id
+    where t1.name is null or t2.salary is null
+)
+select * 
+from new
+order by employee_id;
+
