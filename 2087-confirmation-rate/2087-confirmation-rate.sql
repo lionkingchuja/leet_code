9fcd8e79bb0d5f422a630146df90a11dfa90f253
Vivek Kumar <94731498+lionkingchuja@users.noreply.github.com>
2024-07-17 18:00:10 +0530

    Time: 607 ms (69.52%), Space: 0B (100%) - LeetHub

--- 2087-confirmation-rate/2087-confirmation-rate.sql
+++ 2087-confirmation-rate/2087-confirmation-rate.sql
@@ -1,8 +1,11 @@
 # Write your MySQL query statement below
-select  t1.user_id,round(avg(if(t2.action='confirmed',1,0)),2) as confirmation_rate
-from Signups as t1 
-left join Confirmations as t2
-ON t1.user_id=t2.user_id
-group by t1.user_id;
-# having t2.action='confirmed';
-#where count(t2.action='confirmed') or count(t2.action='timeout');
+with new as
+(
+select t1.user_id,action,count(t2.action) as total_cnt,
+sum(case when action='confirmed' then 1 else 0 end) as cnt
+from Signups as t1 left join Confirmations as t2 on t1.user_id=t2.user_id
+group by t1.user_id
+)
+
+select user_id,if(round(cnt/total_cnt,2) is null,0,round(cnt/total_cnt,2)) as confirmation_rate
+from new;
