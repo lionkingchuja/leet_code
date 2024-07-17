5b7f2d157c0bfc02ef3b89881f8f5dfd97d0a2e2
Vivek Kumar <94731498+lionkingchuja@users.noreply.github.com>
2024-07-17 18:01:45 +0530

    Time: 594 ms (74.62%), Space: 0B (100%) - LeetHub

--- 2087-confirmation-rate/2087-confirmation-rate.sql
+++ 2087-confirmation-rate/2087-confirmation-rate.sql
@@ -7,5 +7,5 @@ from Signups as t1 left join Confirmations as t2 on t1.user_id=t2.user_id
 group by t1.user_id
 )
 
-select user_id,if(round(cnt/total_cnt,2) is null,0,round(cnt/total_cnt,2)) as confirmation_rate
+select user_id,COALESCE(ROUND(cnt / total_cnt, 2), 0) as confirmation_rate
 from new;
