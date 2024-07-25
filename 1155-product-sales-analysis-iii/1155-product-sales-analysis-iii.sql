6b9e25dbbb15199d09346ec5f782c9afc33abc1e
Vivek Kumar <94731498+lionkingchuja@users.noreply.github.com>
2024-07-26 03:04:24 +0530

    Time: 911 ms (85.56%), Space: 0B (100%) - LeetHub

--- 1155-product-sales-analysis-iii/1155-product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/1155-product-sales-analysis-iii.sql
@@ -1,20 +1,20 @@
 # Write your MySQL query statement below
--- with new as
--- (
--- select *,
--- row_number() over (partition by product_id order by year) as rnk
--- from Sales
--- )
--- select * from new;
--- select t1.product_id,t1.year as first_year,t1.quantity,t1.price
--- from new as t1 inner join Product as t2 on t1.product_id=t2.product_id
--- where t1.rnk=1;
+WITH new AS (
+    SELECT *,
+           dense_rank() OVER (PARTITION BY product_id ORDER BY year) AS rnk
+    FROM Sales
+)
+SELECT t1.product_id, t1.year AS first_year, t1.quantity, t1.price
+FROM new AS t1
+WHERE t1.rnk = 1;
 
-select t1.product_id,t1.year as first_year,t1.quantity,t1.price
-from Sales as t1 left join Product as t2 on t1.product_id=t2.product_id
-where (t1.product_id,t1.year) in 
-(
-    select product_id,min(year)
-    from Sales
-    group by product_id
-);
+
+
+-- select t1.product_id,t1.year as first_year,t1.quantity,t1.price
+-- from Sales as t1 left join Product as t2 on t1.product_id=t2.product_id
+-- where (t1.product_id,t1.year) in 
+-- (
+--     select product_id,min(year)
+--     from Sales
+--     group by product_id
+-- );
