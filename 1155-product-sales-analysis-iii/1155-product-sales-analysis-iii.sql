061b9cb35fc0e91133cbf7b90854ade22eed38a5
Vivek Kumar <94731498+lionkingchuja@users.noreply.github.com>
2024-07-26 02:54:49 +0530

    Time: 947 ms (77.08%), Space: 0B (100%) - LeetHub

--- 1155-product-sales-analysis-iii/1155-product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/1155-product-sales-analysis-iii.sql
@@ -1,13 +1,20 @@
 # Write your MySQL query statement below
+-- with new as
+-- (
+-- select *,
+-- row_number() over (partition by product_id order by year) as rnk
+-- from Sales
+-- )
+-- select * from new;
+-- select t1.product_id,t1.year as first_year,t1.quantity,t1.price
+-- from new as t1 inner join Product as t2 on t1.product_id=t2.product_id
+-- where t1.rnk=1;
 
-# product id, year, quantity, and price 
-# first year of every product sold
-
-
-SELECT product_id, year AS first_year, quantity, price
-FROM Sales
-WHERE (product_id, year) in (
-    SELECT product_id, MIN(year) 
-    FROM Sales
-    GROUP BY product_id
+select t1.product_id,t1.year as first_year,t1.quantity,t1.price
+from Sales as t1 left join Product as t2 on t1.product_id=t2.product_id
+where (t1.product_id,t1.year) in 
+(
+    select product_id,min(year)
+    from Sales
+    group by product_id
 );
