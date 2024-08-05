2b79b29934a112c0f94e1d9e0599909696787dbe
Vivek Kumar <94731498+lionkingchuja@users.noreply.github.com>
2024-08-05 14:02:55 +0530

    Time: 7 ms (55.29%), Space: 44.5 MB (17.65%) - LeetHub

--- 2163-kth-distinct-string-in-an-array/2163-kth-distinct-string-in-an-array.java
+++ 2163-kth-distinct-string-in-an-array/2163-kth-distinct-string-in-an-array.java
@@ -4,9 +4,9 @@ class Solution {
         for(int i=0;i<arr.length;i++){
             m.put(arr[i],m.getOrDefault(arr[i], 0) + 1);
         }
-        m.forEach((key,value)->{
-            System.out.println(key+" "+value);
-        });
+        // m.forEach((key,value)->{
+        //     System.out.println(key+" "+value);
+        // });
         int cnt=0;
         for(int i=0;i<arr.length;i++){
             if(m.containsKey(arr[i]) && m.get(arr[i])==1){
