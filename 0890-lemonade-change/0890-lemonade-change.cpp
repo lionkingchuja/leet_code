be6782a1cec92967a6c014252b25a0b0c288d9da
Vivek Kumar <94731498+lionkingchuja@users.noreply.github.com>
2024-08-15 16:11:44 +0530

    Time: 76 ms (38.7%), Space: 86.3 MB (6.78%) - LeetHub

---
+++ 0890-lemonade-change/0890-lemonade-change.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        map<int,int>m;
+        for(auto i:bills){
+            if(i==10){
+                m[10]++;
+                if(m.find(5) != m.end()){
+                    m[5]--;
+                    if(m[5]==0) m.erase(5);
+                }
+                else return false;
+            }
+            else if(i==20){
+                cout<<m[5]<<" "<<m[10]<<endl;
+                if(m[10] >=1 and m[5]>=1){
+                    m[10]--;
+                    m[5]--;
+                    if(m[5]==0) m.erase(5);
+                    if(m[10]==0) m.erase(10);
+                }
+                else if(m.find(5) !=m.end()){
+                    if(m[5] >=3) m[5] -=3;
+                    else return false;
+                    if(m[5]==0) m.erase(5);
+                }
+                else return false;
+            }
+            else m[i]++;
+        }
+        cout<<m[5]<<" "<<m[10]<<endl;
+        return true;
+    }
+};
