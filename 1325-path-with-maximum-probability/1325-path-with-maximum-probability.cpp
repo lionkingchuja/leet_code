ea5b71975e2356a385d29ecfe5f7c83927805e77
Vivek Kumar <94731498+lionkingchuja@users.noreply.github.com>
2024-09-01 00:37:10 +0530

    Time: 126 ms (49.42%), Space: 68.9 MB (69.91%) - LeetHub

--- 1325-path-with-maximum-probability/1325-path-with-maximum-probability.cpp
+++ 1325-path-with-maximum-probability/1325-path-with-maximum-probability.cpp
@@ -1,25 +1,37 @@
 class Solution {
 public:
     double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start_node, int end_node) {
-    vector<pair<int,double>>adj[n];
-    for(int i=0;i<edges.size();i++){
-        adj[edges[i][0]].push_back({edges[i][1],succProb[i]});
-        adj[edges[i][1]].push_back({edges[i][0],succProb[i]});
-    }
-    vector<double>dist(n,0.0);
-    queue<pair<int,double>>q;
-    q.push({start_node,1.0});
-    dist[start_node]=1.0;
-    while(!q.empty()){
-        auto it=q.front();
-        q.pop();
-        for(auto v:adj[it.first]){
-            if(dist[v.first] <it.second *v.second){
-                dist[v.first]= it.second* v.second;
-                q.push({v.first,dist[v.first]});
+        vector<pair<int,double>> vp[n];
+        for(int i = 0; i < edges.size(); i++) {
+            vp[edges[i][0]].push_back({edges[i][1], succProb[i]});
+            vp[edges[i][1]].push_back({edges[i][0], succProb[i]});
+        }
+        
+        vector<double> dis(n, 0.0);
+        priority_queue<pair<double, int>> pq;
+        pq.push({1.0, start_node});
+        dis[start_node] = 1.0;
+        while(!pq.empty()) {
+            auto curr = pq.top();
+            pq.pop();
+            int node = curr.second;
+            double curr_prob = curr.first;
+            // if (node == end_node) {
+            //     return curr_prob;
+            // }
+            // if (curr_prob < dis[node]) continue;
+            
+            for (auto& neighbor : vp[node]) {
+                int next_node = neighbor.first;
+                double next_prob = neighbor.second;
+                
+                if (dis[next_node] < curr_prob * next_prob) {
+                    dis[next_node] = curr_prob * next_prob;
+                    pq.push({dis[next_node], next_node});
+                }
             }
         }
-    }
-    return dist[end_node];
+        
+        return dis[end_node];
     }
 };
