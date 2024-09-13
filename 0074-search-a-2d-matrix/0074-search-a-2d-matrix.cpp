aad7cf426fa929e8d8af36711b0457442adcc202
Vivek Kumar <94731498+lionkingchuja@users.noreply.github.com>
2024-09-13 18:35:19 +0530

    Time: 0 ms (100%), Space: 12.3 MB (43.08%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -22,14 +22,12 @@ public:
         return false;
     }
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        // if(matrix[0][0] ==target) return true;
         int r=-1;
         if(matrix.size()==1){
             r=0;
         }
         else r=binary_search(matrix,target);
 
-        cout<<r<<endl;
         vector<int>v;
         for(int i=0;i<matrix.size();i++){
             if(i==r){
