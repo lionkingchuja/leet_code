e14492d9181dc09cf9b6792da7045af62a24f4e7
Vivek Kumar <94731498+lionkingchuja@users.noreply.github.com>
2024-10-01 10:26:01 +0530

    Time: 65 ms (5.04%), Space: 50.2 MB (5.57%) - LeetHub

--- 1497-design-a-stack-with-increment-operation/1497-design-a-stack-with-increment-operation.cpp
+++ 1497-design-a-stack-with-increment-operation/1497-design-a-stack-with-increment-operation.cpp
@@ -1,33 +1,60 @@
 class CustomStack {
 public:
-    int n;
-    stack<int> stack;
-    vector<int> inc;
-
-    CustomStack(int n) {
-        this->n = n;
+    stack<int>st;
+    int len;
+    int cnt;
+    CustomStack(int maxSize) {
+        len=maxSize;
+        cnt=0;
     }
-
+    
     void push(int x) {
-        if (stack.size() < n) {
-            stack.push(x);
-            inc.push_back(0);
-        }
+        if(cnt >=len) return;
+        st.push(x);
+        cnt++;
     }
-
+    
     int pop() {
-        if (stack.empty()) return -1;
-        if (inc.size() > 1) inc[inc.size() - 2] += inc.back();
-        int res = stack.top() + inc.back();
-        stack.pop();
-        inc.pop_back();
-        return res;
-    }
-
-    void increment(int k, int val) {
-        if (!stack.empty()) {
-            int idx = min(k, (int)inc.size()) - 1;
-            inc[idx] += val;
+        if(!st.empty()){
+            int x=st.top();
+            st.pop();
+            cnt--;
+            return x;
         }
+        else return -1;
+    }
+    
+    void increment(int k, int val) {
+        if(st.empty()) return;
+        vector<int>v;
+        if(k >= len){
+            while(!st.empty()){
+                v.push_back(st.top()+val);
+                st.pop();
+            }
+            reverse(v.begin(),v.end());
+            for(auto i:v) st.push(i);
+        }
+        else{
+            while(!st.empty()){
+                v.push_back(st.top());
+                st.pop();
+            }
+            reverse(v.begin(),v.end());
+            for(int i=0;i<v.size();i++){
+                if(i<k) st.push(v[i]+val);
+                else st.push(v[i]);
+            }
+        }
+
+        
     }
 };
+
+/**
+ * Your CustomStack object will be instantiated and called as such:
+ * CustomStack* obj = new CustomStack(maxSize);
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * obj->increment(k,val);
+ */
