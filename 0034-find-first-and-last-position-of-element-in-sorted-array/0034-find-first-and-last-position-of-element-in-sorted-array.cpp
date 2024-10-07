f6cc4bd0ad5bc7c03094bb87aae0acc8766dc134
Vivek Kumar <94731498+lionkingchuja@users.noreply.github.com>
2024-10-08 00:00:59 +0530

    Time: 7 ms (47.72%), Space: 16.1 MB (99.44%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,36 +1,21 @@
 class Solution {
 public:
-    int binary_search(vector<int>&nums,int target,bool flag){
-        int l=0;
-        int r=nums.size()-1;
-        int index=-1;        
-        while(l<=r){
-            int mid=(l+r)/2;
-            if(nums[mid]==target){
-                index=mid;
-                if(flag) r=mid-1;
-                else l=mid+1;
-            }
-            else if(nums[mid]> target){
-                r=mid-1;
-            }
-            else if(nums[mid] <target){
-                l=mid+1;
-            }
+    int solve(vector<int>&nums,int target){
+        int low=0,high=nums.size()-1;
+        while(low <=high){
+            int mid=(low+high)/2;
+            if(nums[mid]==target) return mid;
+            else if(nums[mid] > target) high=mid-1;
+            else low=mid+1;
         }
-        return index;
+        return -1;
     }
     vector<int> searchRange(vector<int>& nums, int target) {
-        int ans=binary_search(nums,target,true);
-        int ans1=binary_search(nums,target,false);
-        if(ans==-1){
-            return{-1,-1};
-        }
-        else if(ans==ans1){
-            return{ans,ans};
-        }
-        else{
-            return{ans,ans1};
-        }
+        if(nums.size()==0) return {-1,-1};
+        int find=solve(nums,target);
+        if(find==-1) return{-1,-1};
+        int x=lower_bound(nums.begin(),nums.end(),target)-nums.begin();
+        int y=upper_bound(nums.begin(),nums.end(),target)-nums.begin();
+        return {x,y-1};
     }
 };
