156d872dbbaeb360b3f4831374c53b04e38cb7c9
Vivek Kumar <94731498+lionkingchuja@users.noreply.github.com>
2024-10-15 19:55:00 +0530

    Time: 39 ms (21.05%), Space: 15.7 MB (45.68%) - LeetHub

--- 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.cpp
+++ 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
     long long minimumSteps(string s) {
-        int cnt1=0;
-        for(auto i:s){
-            if(i=='1') cnt1++;
-        }
-        vector<int>v1,v2;
+        int i=0;
         int j=s.size()-1;
-        int len=cnt1;
-        while(len--){
-            if(s[j]=='0') v1.push_back(j);
-            j--;
-        }
-        
-        while(j>=0){
-            if(s[j]=='1') v2.push_back(j);
-            j--;
-        }
-        cout<<v1.size();
-        cout<<v2.size();
         long long ans=0;
-        for(int i=0;i< v1.size();i++){
-            ans +=abs(v1[i]-v2[i]);
+        bool flag1=false;
+        bool flag2=false;
+        while(i<j){
+            if(s[i]=='0') i++;
+            else flag1=true;
+            if(s[j]=='1') j--;
+            else flag2=true;
+            if(flag1==true and flag2==true){
+                ans +=j-i;
+                flag1=false;
+                flag2=false;
+                i++;
+                j--;
+            }
         }
         return ans;
     }
