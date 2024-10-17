1c8b8a5597270a1189b6e8b6715216caced0ebc1
Vivek Kumar <94731498+lionkingchuja@users.noreply.github.com>
2024-10-17 18:53:44 +0530

    Time: 0 ms (100%), Space: 7.6 MB (31.77%) - LeetHub

---
+++ 0670-maximum-swap/0670-maximum-swap.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maximumSwap(int num) {
+        string s=to_string(num);
+        int max_index=s.size()-1;
+        int swap1=-1;
+        int swap2=-1;
+        for(int i=s.size()-2;i >=0;i--){
+            if(max_index ==-1 or s[i] > s[max_index]){
+                max_index=i;
+            }
+            else if(s[i] < s[max_index]){
+                swap1=i;
+                swap2=max_index;
+            }
+        }
+        if(swap1 != -1 and swap2 != -1) swap(s[swap1],s[swap2]);
+        return stoi(s);
+    }
+};
