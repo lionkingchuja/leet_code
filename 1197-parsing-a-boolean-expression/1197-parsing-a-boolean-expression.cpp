dd44c3e0df4e4d5e790a11d1bf263fb48e4dd577
Vivek Kumar <94731498+lionkingchuja@users.noreply.github.com>
2024-10-21 01:26:11 +0530

    Time: 9 ms (53.55%), Space: 11.3 MB (41%) - LeetHub

---
+++ 1197-parsing-a-boolean-expression/1197-parsing-a-boolean-expression.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    bool parseBoolExpr(string s) {
+    stack<char> st1;  // Operator stack
+    stack<char> st2;  // Value stack
+    
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == '&' || s[i] == '|' || s[i] == '!') {
+            st1.push(s[i]);  // Push operators onto the operator stack
+        }
+        else if (s[i] == '(') {
+            st2.push(s[i]);  // Push opening bracket onto the value stack
+        }
+        else if (s[i] == ')') {
+            // Process the current expression enclosed by '(' and ')'
+            char op = st1.top();
+            st1.pop();
+            vector<char> vals;
+            
+            // Collect all values between '(' and ')'
+            while (st2.top() != '(') {
+                vals.push_back(st2.top());
+                st2.pop();
+            }
+            st2.pop();  // Pop the '('
+            
+            // Perform the operation based on the operator
+            char result = 't';
+            if (op == '&') {
+                result = 't';
+                for (char c : vals) {
+                    if (c == 'f') {
+                        result = 'f';
+                        break;
+                    }
+                }
+            } 
+            else if (op == '|') {
+                result = 'f';
+                for (char c : vals) {
+                    if (c == 't') {
+                        result = 't';
+                        break;
+                    }
+                }
+            } 
+            else if (op == '!') {
+                result = (vals[0] == 't') ? 'f' : 't';
+            }
+            
+            // Push the result back onto the value stack
+            st2.push(result);
+        } 
+        else {
+            st2.push(s[i]);  // Push boolean values ('t' or 'f') onto the value stack
+        }
+    }
+    
+    // The final result will be at the top of the value stack
+    return st2.top() == 't';
+}
+
+};
